905c7f03bb9bfd62e15bf8bef14888a91bfe63b5
Tom Haney <tomhaney@protonmail.com>
2022-10-12 13:24:28 -0500

    ruby project creation work

---
+++ Rakefile.rb
@@ -0,0 +1,26 @@
+require "rake/testtask"
+require 'find'
+
+desc 'Say hello'
+task :hello do
+  puts "Hello there. This is the 'hello' task."
+end
+
+desc 'Run tests'
+task :default => :test
+
+Rake::TestTask.new(:test) do |t|
+  t.libs << "test"
+  t.libs << "lib"
+  t.test_files = FileList['test/**/*_test.rb']
+end
+
+desc 'List project files'
+task :listfiles do
+  files = []
+  Find.find('.') do |path|
+    next if path.include?('/.')
+    files << path if File.file?(path)
+  end
+  puts files
+end
